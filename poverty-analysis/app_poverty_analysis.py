aec0d23db778df905e3e32e253da1cf6d847ab87
Andrii Adamchuk <andrii.adamchuk@gmail.com>
2022-01-13 21:42:06 +0100

    Update app_poverty_analysis.py

--- poverty-analysis/app_poverty_analysis.py
+++ poverty-analysis/app_poverty_analysis.py
@@ -364,4 +364,9 @@ def plot_poverty_gap_chart(year, indicator):
 
 # running the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port=8080, use_reloader=False)
+    #app.run_server(debug=True, host="localhost", port=8080, use_reloader=False)
+
+
+
+
